6c552e35454089e93b296acc32787ba811179bb4
Luís Simas <luissimas@protonmail.com>
2022-06-13 16:16:13 -0300

    feat: generating reports in range

--- lib/bookings/report.ex
+++ lib/bookings/report.ex
@@ -3,16 +3,35 @@ defmodule Flightex.Bookings.Report do
   alias Flightex.Bookings.Booking
 
   def generate(filename \\ "report.csv") do
-    bookings = build_bookings()
+    bookings =
+      BookingAgent.list()
+      |> Map.values()
+      |> build_bookings()
 
     File.write(filename, bookings)
   end
 
-  defp build_bookings() do
-    BookingAgent.list()
-    |> Map.values()
-    |> Enum.map(&stringify_booking/1)
+  def generate(from_date, to_date, filename \\ "report.csv") do
+    bookings =
+      BookingAgent.list()
+      |> Map.values()
+      |> Enum.filter(fn %Booking{complete_date: date} -> is_between?(date, from_date, to_date) end)
+      |> build_bookings()
+
+    File.write(filename, bookings)
   end
+
+  defp is_between?(target, start_date, end_date) do
+    case {Date.compare(target, start_date), Date.compare(target, end_date)} do
+      {:gt, :lt} -> true
+      {:gt, :eq} -> true
+      {:eq, :lt} -> true
+      {:eq, :eq} -> true
+      _ -> false
+    end
+  end
+
+  defp build_bookings(bookings), do: Enum.map(bookings, &stringify_booking/1)
 
   defp stringify_booking(%Booking{
          user_id: user_id,
@@ -20,6 +39,6 @@ defmodule Flightex.Bookings.Report do
          local_destination: local_destination,
          complete_date: complete_date
        }) do
-    "#{user_id},#{local_origin},#{local_destination},#{complete_date}"
+    "#{user_id},#{local_origin},#{local_destination},#{complete_date}\n"
   end
 end
--- lib/flightex.ex
+++ lib/flightex.ex
@@ -1,4 +1,5 @@
 defmodule Flightex do
+  alias Flightex.Bookings.Report
   alias Flightex.Users.Agent, as: UserAgent
   alias Flightex.Bookings.Agent, as: BookingAgent
   alias Flightex.Users.CreateOrUpdate, as: CreateOrUpdateUser
@@ -11,4 +12,6 @@ defmodule Flightex do
 
   defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
   defdelegate create_or_update_booking(params), to: CreateOrUpdateBooking, as: :call
+  defdelegate generate_report(filename), to: Report, as: :generate
+  defdelegate generate_report(from_date, to_date, filename), to: Report, as: :generate
 end
